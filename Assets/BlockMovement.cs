18e432f2ff0568acbc20dcf89aa03f48f653d7cd
Ralph N <ralph.nevchas@gmail.com>
2015-11-01 17:44:52 -0800

    basic grid collision done

--- Assets/BlockMovement.cs
+++ Assets/BlockMovement.cs
@@ -10,8 +10,7 @@ public class BlockMovement : MonoBehaviour {
 	float defaultFallSpeed = 1f;
     float fallSpeed = 1f;
 
-	float currentDepth = 1f;
-	float nextDepth = 2f;
+	float zNextCheckPoint = 1f;
 
     bool isFalling = true;
     bool isMoving = false;
@@ -23,6 +22,8 @@ public class BlockMovement : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
+
+		initGrid();
         // get the active block
         //activeBlock = GameObject.FindGameObjectWithTag("Player");
 		activeBlock = (GameObject)GameObject.Instantiate(BLOCK, new Vector3(-2, -2, 1), Quaternion.identity);
@@ -37,19 +38,35 @@ public class BlockMovement : MonoBehaviour {
         {
             if (Input.GetKeyDown(KeyCode.LeftArrow))
             {
-                SmoothMove(activeBlock.transform.position, activeBlock.transform.position + new Vector3(-1, 0, 0));
+				Vector3 futurePos = activeBlock.transform.position + new Vector3(-1, 0, 0);
+				Quaternion futureRot = activeBlock.transform.rotation;
+				if (!IsPositionBlocked(futurePos, futureRot)) {
+					SmoothMove(activeBlock.transform.position, futurePos);
+				}
             }
             else if (Input.GetKeyDown(KeyCode.RightArrow))
             {
-                SmoothMove(activeBlock.transform.position, activeBlock.transform.position + new Vector3(1, 0, 0));
+				Vector3 futurePos = activeBlock.transform.position + new Vector3(1, 0, 0);
+				Quaternion futureRot = activeBlock.transform.rotation;
+				if (!IsPositionBlocked(futurePos, futureRot)) {
+					SmoothMove(activeBlock.transform.position, futurePos);
+				}
             }
             else if (Input.GetKeyDown(KeyCode.UpArrow))
             {
-                SmoothMove(activeBlock.transform.position, activeBlock.transform.position + new Vector3(0, 1, 0));
+				Vector3 futurePos = activeBlock.transform.position + new Vector3(0, 1, 0);
+				Quaternion futureRot = activeBlock.transform.rotation;
+				if (!IsPositionBlocked(futurePos, futureRot)) {
+					SmoothMove(activeBlock.transform.position, futurePos);
+				}
             }
             else if (Input.GetKeyDown(KeyCode.DownArrow))
             {
-                SmoothMove(activeBlock.transform.position, activeBlock.transform.position + new Vector3(0, -1, 0));
+				Vector3 futurePos = activeBlock.transform.position + new Vector3(0, -1, 0);
+				Quaternion futureRot = activeBlock.transform.rotation;
+				if (!IsPositionBlocked(futurePos, futureRot)) {
+					SmoothMove(activeBlock.transform.position, futurePos);
+				}
             }
 			else if (Input.GetKeyDown(KeyCode.Q))
 			{
@@ -96,22 +113,60 @@ public class BlockMovement : MonoBehaviour {
         }
         if (isFalling)
         {
-            activeBlock.transform.position += new Vector3(0, 0, fallSpeed * Time.deltaTime);
-			if (activeBlock.transform.position.z >= nextDepth) {
-				currentDepth = nextDepth;
-				nextDepth++;
+			activeBlock.transform.position += new Vector3(0, 0, fallSpeed * Time.deltaTime);
+			if (activeBlock.transform.position.z >= zNextCheckPoint) {
+				zNextCheckPoint++;
 
-				if (currentDepth == 10)
+				Vector3 futurePos = activeBlock.transform.position + new Vector3(0, 0, 1);
+				Quaternion futureRot = activeBlock.transform.rotation;
+				if (IsPositionBlocked(futurePos, futureRot))
 				{
-					activeBlock.transform.position = new Vector3(activeBlock.transform.position.x, activeBlock.transform.position.y, 10);
+					activeBlock.transform.position = new Vector3(
+						(int)Mathf.Round(activeBlock.transform.position.x), 
+						(int)Mathf.Round(activeBlock.transform.position.y), 
+						(int)Mathf.Round(activeBlock.transform.position.z));
+					SetPositionBlocked();
+
 					activeBlock = (GameObject)GameObject.Instantiate(BLOCK, new Vector3(-2, -2, 1), Quaternion.identity);
-					currentDepth = 1;
-					nextDepth = 2;
+					zNextCheckPoint = 1;
 				}
 			}
         }
 
     }
+
+	bool[,,] blocked = new bool[7,7,12];
+
+	private void initGrid() {
+		for (int i = 0; i < 7; i++) {
+			for (int j = 0; j < 7; j++) {
+				for (int k = 0; k < 12; k++) {
+					blocked[i,j,k] = false;
+					if (i == 0 || j == 0 || k == 0 || i == 6 || j == 6 || k == 11)
+					{
+						blocked[i,j,k] = true;
+					}
+				}
+			}
+		}
+	}
+
+	private bool IsPositionBlocked(Vector3 futurePos, Quaternion futureRot) {
+
+		if (blocked[
+			(int)Mathf.Round (futurePos.x) + 3, (int)Mathf.Round (futurePos.y) + 3, (int)Mathf.Round (futurePos.z)])
+			return true;
+		return false;
+	}
+
+	private void SetPositionBlocked() // uses activeBlock
+	{
+		int i = (int)Mathf.Round (activeBlock.transform.position.x) + 3;
+		int j = (int)Mathf.Round (activeBlock.transform.position.y) + 3;
+		int k = (int)Mathf.Round (activeBlock.transform.position.z);
+		blocked[i,j,k] = true;
+
+	}
 
     private void SmoothMove(Vector3 start, Vector3 end)
     {
